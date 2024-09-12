4de82be7a63eb470f8fb0145133288ef11ad2843
yeisimiranda <yeisimiranda@hotmail.comgit config --global user.emailgit config --global user.email yeisimiranda@hotmail.com>
2024-09-12 15:20:59 -0600

    actualizado_con_header

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ import plotly_express as ply
 
 car_data = pd.read_csv("vehicles_us.csv")
 
-print(car_data.head())
+st.header("Vehicles Sales in US")
+st.write("Esta aplicación aún no es funcional. En construcción")
