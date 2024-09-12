5c791381bd03d984ee1e5faa6fa1d4a7fd8519d1
yeisimiranda <yeisimiranda@hotmail.comgit config --global user.emailgit config --global user.email yeisimiranda@hotmail.com>
2024-09-12 17:03:11 -0600

    prueba

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as ply
+import plotly.express as px
 
-car_data = pd.read_csv("vehicles_us.csv")
+car_data = pd.read_csv("\vehicles_us.csv")
 
-st.header("Vehicles Sales in US")
-st.write("Esta aplicación aún no es funcional. En construcción")
+st.header("Anucnios de Ventas de Coches en USA")
+
+hist_button = st.button("Crea Histograma")
+
+if hist_button:
+    st.write(
+        "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button("Crear Gráfica Dispersión")
+
+if scatter_button:
+    st.write("Creación de una gráfica de dispersión para el conjunto de datos de anuncios de venta de coches")
+    fig_sactter = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_sactter, use_container_width=True)
